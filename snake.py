ba0e84cb0ed9692b2f3c479b64965550e6a6b86b
Samim225 <najibasultani1682@gmail.com>
2025-09-16 22:01:04 +0430

    snake gamim

--- snake.py
+++ snake.py
@@ -1,14 +1,17 @@
 from tkinter import *
 import random
-
+window = Tk()
+window.title("samim game best develaoper in the world hahha")
+window.resizable(False, False)
 GAME_WIDTH = 700
-GAME_HIEGHT = 500
+GAME_HIEGHT = 700
 SPEED = 20
-SPACE_SIZE = 10
+SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#39F900"
-FOOD = "#FFF133"
-BACKGROUND = "#F131F4"
+FOOD_COLOR = "#FFF133"
+BACKGROUND = "#31BDF4"
+#adddesd
 
 
 
@@ -16,6 +19,15 @@ class Snake:
     pass
 class Food:
     pass
+    def __init__(self):
+        x = random.randint(0, GAME_WIDTH // SPACE_SIZE -1) * SPACE_SIZE
+        y = random.randint(0, GAME_HIEGHT // SPACE_SIZE -1) * SPACE_SIZE
+        self.coordin = [y,x]
+        canva.create_oval(
+            x,y, x + SPACE_SIZE, y + SPACE_SIZE,
+            fill = FOOD_COLOR, tag ="food"
+        )
+
 def next_turn():
     pass
 def change_dierect():
@@ -24,7 +36,19 @@ def check_collisions():
     pass
 def game_over():
     pass
-window = Tk()
-window.title("samim game best develaoper in the world hahha")
-window.resizable(False, False)
+
+score = 0
+label = Label(window, text="Score {}".format(score), font=("consolas",40))
+label.pack()
+
+canva = Canvas(window, bg=BACKGROUND, width=GAME_WIDTH , height=GAME_HIEGHT)
+canva.pack()
+snake = Snake()
+food = Food()
+# window.update()
+# window_height = window.winfo_height()
+# window_width = window.winfo_width()
+# screen_width = window.winfo_screenwidth()
+# screen_height = window.winfo_screenheight()
+
 window.mainloop()
